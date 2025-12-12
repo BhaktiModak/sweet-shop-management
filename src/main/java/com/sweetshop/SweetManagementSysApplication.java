9f1c1fb8ef1dffeaac9948aa90ef3b8358f5b803
BhaktiModak <103882747+BhaktiModak@users.noreply.github.com>
2025-12-12 22:53:45 +0530

    initial spring boot project setup

---
+++ src/main/java/com/sweetshop/SweetManagementSysApplication.java
@@ -0,0 +1,13 @@
+package com.sweetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetManagementSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetManagementSysApplication.class, args);
+	}
+
+}
